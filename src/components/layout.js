473ad8d1c3be81dbec2bb29ac19219cebc6eab66
Matthew Thomson <matthewthomson.nz@gmail.com>
2020-07-17 16:02:26 +1200

    troubleshooting prod

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@ import VerticalPositioner from './verticalPositioner'
 export default withTheme(function Layout({children, data, path }) {
 
     const pageData = data.allStrapiPage.edges[0].node;
-    console.log("aa"+path+"aa")
+
     const nav = useStaticQuery(graphql`
             query LayoutQuery {
                 allStrapiPage(sort: {fields: pageOrder}) {
@@ -47,11 +47,11 @@ export default withTheme(function Layout({children, data, path }) {
             </Link>
                 <ul>
             {navLinks.map((value, index) => {
-              console.log("aa" + path + "aa")
-              console.log("bb" + value.node.Path + "bb")
-              console.log("cc" + value.node.Path === path  + "cc")
-              console.log("cc" + value.node.Path == path + "cc")
-              return <li className={value.node.Path === path ? "active": ""}><Link to={value.node.Path}>{value.node.Title}</Link></li>
+              // console.log("aa" + path + "aa")
+              // console.log("bb" + value.node.Path + "bb")
+              console.log(value.node.Path.charCodeAt(0) === path.charCodeAt(0), value.node.Path.charCodeAt(0),path.charCodeAt(0) )
+              // console.log("cc" + value.node.Path == path + "cc")
+              return <li className={value.node.Path.charCodeAt(0) === path.charCodeAt(0) ? "active": ""}><Link to={value.node.Path}>{value.node.Title}</Link></li>
             })}
                 </ul>
             </header>
